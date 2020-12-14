a8fa796a790e1aae973a378740e66799980143eb
ghostxbh <ghostxbh@hotmail.com>
2020-12-14 23:57:56 +0800

    add  common

---
+++ src/main/java/com/uzykj/chinatruck/common/Constants.java
@@ -0,0 +1,14 @@
+package com.uzykj.chinatruck.common;
+
+/**
+ * @author ghostxbh
+ */
+public interface Constants {
+    String brand = "brand";
+
+    String category = "category";
+
+    String contact = "contact";
+
+    String parts_info = "parts_info";
+}
---
+++ src/main/java/com/uzykj/chinatruck/common/ResponseContants.java
@@ -0,0 +1,17 @@
+package com.uzykj.chinatruck.common;
+
+/**
+ * @author ghostxbh
+ */
+public interface ResponseContants {
+
+    String cust_name = "缺少客户名称";
+
+    String cust_phone = "缺少客户电话";
+
+    String cust_email = "缺少客户邮箱";
+
+    String brand = "缺少零件品牌";
+
+    String part_desc = "缺少零件描述";
+}
